37ac7096836daeccc7a23f3ab19d71fc4e2c1d80
Roshan <shahroshan0720@gmail.com>
2023-10-07 19:33:12 +0530

    rate limit setup

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const rateLimit = require('express-rate-limit');
 const router = require('./routes/index');
 const errorHandler = require('./middlewares/error');
 const app = express();
 
 app.use(cors());
+
+const limiter = rateLimit({
+    windowMs: 10*60*1000,
+    max: 10
+});
+
+app.use(limiter);
+app.set('trust proxy',1)
 
 app.use(express.static('public'));
 
