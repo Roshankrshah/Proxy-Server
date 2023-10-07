bb5eeb0511b3589afa1086d180c9df77b09c434c
Roshan <shahroshan0720@gmail.com>
2023-10-07 19:40:21 +0530

    cache setup

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,15 @@ const url = require('url');
 const express = require('express');
 const router = express.Router();
 const axios = require('axios');
+const apicache = require('apicache');
 
 const API_KEY_VALUE = process.env.API_KEY_VALUE
 const API_KEY_NAME = process.env.API_KEY_NAME
 const API_BASE_URL = process.env.API_BASE_URL
 
-router.get('/',async(req,res,next)=>{
+let cache = apicache.middleware;
+
+router.get('/',cache('2 minutes'),async(req,res,next)=>{
     try{
         const params = new URLSearchParams({
             [API_KEY_NAME]: API_KEY_VALUE,
